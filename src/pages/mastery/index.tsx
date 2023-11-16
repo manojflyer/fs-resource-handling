98b158c56608ba9a5b5ed6a3a28b5c0a5cb751d5
Manoj Surabathulla <109808124+manoj-surabathulla@users.noreply.github.com>
2023-11-16 22:01:30 +0530

    Feature/fs 08 icons storybook (#10)
    * icons and implemented storybook
    
    * deleted logo image from icons
    
    ---------
    
    Co-authored-by: Manoj Surabathulla <manojsurabathulla@Manoj-Surabathulla-2.local>

--- src/pages/mastery/index.tsx
+++ src/pages/mastery/index.tsx
@@ -1,5 +1,11 @@
+import { SearchIcon } from "../../assets/icons";
+
 const Mastery = () => {
-  return <div className="text-xl font-Lato text-gray-textGray">Mastery</div>;
+  return (
+    <div className="text-xl font-Lato text-gray-textGray">
+      Mastery <SearchIcon className="fill-[white]" />
+    </div>
+  );
 };
 
 export default Mastery;
